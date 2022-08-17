59b468b2008e7e728c3de71d0b0b1fa2f06f7630
AzamatXakimov <typetiger59@gmail.com>
2022-08-17 23:02:15 +0500

    Creatplan page finish

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 var elNavBtn = document.querySelector(".js-nav-open-btn");
+var elOpenModalBtn = document.querySelector(".js-open-modal");
+var elModal = document.querySelector(".modal");
+var elCloseModalBtnM = document.querySelector(".js-close-m-btn");
+var elCloseModalBtn = document.querySelector(".js-close-btn") 
 
 elNavBtn.addEventListener("click", function(){
     elNavBtn.closest(".site-header").classList.toggle("sitenav-open");
 })
+
+elOpenModalBtn.addEventListener("click", function(){
+    elModal.classList.add("modal--open");
+    elModal.closest(".site-body").classList.add("modal-opened");
+});
+
+elCloseModalBtnM.addEventListener("click", function(){
+    elModal.classList.remove("modal--open");
+    elModal.closest(".site-body").classList.remove("modal-opened");
+});
+
+elCloseModalBtn.addEventListener("click", function(){
+    elModal.classList.remove("modal--open");
+    elModal.closest(".site-body").classList.remove("modal-opened");
+});
